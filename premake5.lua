d21336f205a020e745fe04ffb16bcb6b07f652da
Mikko Määttä <mikkomaatta@Mikko-MacBook-Pro.local>
2025-03-11 00:22:32 +0200

    premake5.lua now working

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "ImGui"
+project "Box2D"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++11"
@@ -10,34 +10,53 @@ project "ImGui"
 	files
 	{
 		"src/**.h",
-		"src/**.cpp",
+		"src/**.c",
 		"include/**.h"
 	}
-	
+
 	includedirs
 	{
 		"include",
 		"src"
 	}
 
+    -- SIMD tuki
+    filter "options:BOX2D_ENABLE_SIMD=true"
+        defines { "BOX2D_ENABLE_SIMD" }
+
+    -- AVX2 tuki x86_64:llä
+    filter "options:BOX2D_AVX2=true"
+        defines { "BOX2D_AVX2" }
+
 	filter "system:macosx"
-		systemversion "15.0"
+		systemversion "12.0"	
+        -- Apple erikoisasetukset (esim. sanitointi)
+        buildoptions { "-fsanitize=address", "-fsanitize=undefined" }
+        linkoptions { "-fsanitize=address", "-fsanitize=undefined" }
 
 	filter "system:windows"
 		systemversion "latest"
+		-- MSVC erityisasetukset
+        buildoptions { "/MP", "/Zi" } -- paralleelit, debug-tiedostot
+        linkoptions { "/INCREMENTAL:NO" }
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
+		buildoptions { "-ffp-contract=off" }
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
+		defines { "DEBUG", "_DEBUG" }
 
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+		defines { "NDEBUG" }
 
-	filter "configuration:Dist"
+	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
+        symbols "off"
+		defines { "NDEBUG" }
