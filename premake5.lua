318eee07223f1971bd7b1833ccbfec9a05c31c79
Mikko Määttä <mikkomaatta@Mikko-MacBook-Pro.local>
2025-03-10 16:33:43 +0200

    premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+	
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:macosx"
+		systemversion "15.0"
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configuration:Dist"
+		runtime "Release"
+		optimize "on"
